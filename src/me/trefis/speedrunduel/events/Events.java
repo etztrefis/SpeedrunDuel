1165ed3717b393efbe6e7cd06b9821ae9314d63c
etztrefis <etztrefishello@gmail.com>
2021-02-13 17:23:50 +0700

    error handling on dealDamage event and saving player role onLeave

--- src/me/trefis/speedrunduel/events/Events.java
+++ src/me/trefis/speedrunduel/events/Events.java
@@ -2,6 +2,7 @@ package me.trefis.speedrunduel.events;
 
 import me.trefis.speedrunduel.PlayerData;
 import me.trefis.speedrunduel.TeamManager;
+import me.trefis.speedrunduel.context.Roles;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,11 +18,14 @@ import org.bukkit.plugin.Plugin;
 import java.util.HashMap;
 import java.util.UUID;
 
+import static org.bukkit.Bukkit.getLogger;
+
 public class Events implements Listener {
     private final PlayerData playerData;
     private final TeamManager teamManager;
     private final Plugin plugin;
     private HashMap<UUID, Integer> deathsMap = new HashMap<UUID, Integer>();
+    private HashMap<UUID, Roles> leaversMap = new HashMap<UUID, Roles>();
 
     public Events(PlayerData playerData, TeamManager teamManager, Plugin plugin) {
         this.playerData = playerData;
@@ -31,23 +35,48 @@ public class Events implements Listener {
 
     @EventHandler
     public void onDealDamage(EntityDamageByEntityEvent event) {
-        Player damager = (Player) event.getDamager();
-        Player entity = (Player) event.getEntity();
-        if (playerData.getRole(damager) == playerData.getRole(entity)) {
-            event.setCancelled(true);
+        try{
+            Player damager = (Player) event.getDamager();
+            Player entity = (Player) event.getEntity();
+            if (playerData.getRole(damager) == playerData.getRole(entity)) {
+                event.setCancelled(true);
+            }
+        }catch (Exception e){
+            getLogger().info("Error while checking on entity damage event | " + e.getMessage());
         }
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
         Player player = event.getPlayer();
+        if(playerData.getRole(player) == Roles.LAVA || playerData.getRole(player) == Roles.WATER){
+            leaversMap.put(player.getUniqueId(), playerData.getRole(player));
+            removePlayer(player, playerData.getRole(player));
+        }
         event.setQuitMessage(ChatColor.RED + player.getName() + " has logged out.");
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        if(leaversMap.containsKey(player.getUniqueId())){
+            addPlayer(player, leaversMap.get(player.getUniqueId()));
+            leaversMap.remove(player.getUniqueId());
+        }
         event.setJoinMessage(ChatColor.GREEN + player.getName() + " has joined.");
+    }
+
+    private void addPlayer(Player player, Roles role) {
+        playerData.setRole(player, role);
+        teamManager.addPlayer(role, player);
+        player.getInventory().remove(Material.COMPASS);
+        player.getInventory().addItem(new ItemStack(Material.COMPASS));
+    }
+
+    private void removePlayer(Player player, Roles role) {
+        playerData.reset(player);
+        teamManager.removePlayer(role, player);
+        player.getInventory().remove(Material.COMPASS);
     }
 
     @EventHandler
